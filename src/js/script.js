7874963a4152589736a07b26ec0a3db673fea987
igor <sdsdds@dd.dd>
2021-01-12 16:47:26 +0200

    tabs 12/01/21

--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,19 @@ $(document).ready(function(){
             breakpoint: 992,
             settings: {
             dots: true,
-            arrows: false
+            arrows: false,
+            dotsClass: 'slick-dots'
+            }
+          },
+          {
+            breakpoint: 425,
+            settings: {
+            dots: true,
+            arrows: false,
+            dotsClass: 'slick-dots'
             }
           }
+          
         ]
     });
 
@@ -22,6 +32,16 @@ $(document).ready(function(){
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    $
-    
-  });
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+      });
+  };
+});
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+  
